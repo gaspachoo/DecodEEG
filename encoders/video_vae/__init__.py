650ef8f9f91f4491ea3a48c70ed07df516f49fdc
Gaspachoo <gaspard.lefort@outlook.fr>
2025-07-21 15:24:29 +0530

    remove some files, move other

--- encoders/video_vae/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Utilities for the video VAE."""
--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Utility modules for EEG-to-Video project."""
-
-from .align import load_aligned_latents, stack_eeg_windows
-
-__all__ = [
-    "stack_eeg_windows",
-    "load_aligned_latents",
-]
